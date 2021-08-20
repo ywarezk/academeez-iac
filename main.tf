8c90eddd4142632c87f7a83efbd1e35918ea121f
ywarezk <ywarezk@gmail.com>
2021-08-20 10:34:15 +0300

    main: created the root folder

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@
 
 provider "google-beta" {
   region      = var.region
-  user_project_override = true
 }
 
 /**
@@ -22,4 +21,21 @@ module "root_folder" {
   parent  = var.org_id
 
   names = [ "academeez" ]
+
+  # list of admins for a specific folder
+  # per_folder_admins = {}
+
+  # these admins will have admin for all the folders and subfolders
+  # you can also set a group here
+  all_folder_admins = [
+    "yariv@nerdeez.com"
+  ]
+
+  #  Use this to add more roles to folder admins
+  # folder_admin_roles = [
+  #   "roles/owner",
+  #   "roles/resourcemanager.folderViewer",
+  #   "roles/resourcemanager.projectCreator",
+  #   "roles/compute.networkAdmin"
+  # ]
 }
---
+++ variables.tf
@@ -0,0 +1,22 @@
+/**
+ * Define the main variables here
+ *
+ * Created August 19th, 2021
+ * @author: ywarezk
+ * @version: 0.0.1
+ */
+
+variable "region" {
+  description = "The region where the infastructure will be"
+  type = string
+}
+
+variable "org_id" {
+  description = "The id of the organization"
+  type = string
+}
+
+variable "billing_account" {
+  description = "Billing accound of the infastructure"
+  type = string
+}
